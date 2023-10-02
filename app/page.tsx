5bcb972afe5100e94bbb4342c701d22695483261
Shippu Kumar <117848490+shibu931@users.noreply.github.com>
2023-10-02 22:22:17 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './globals.css'
 import Image from 'next/image'
 import SearchBar from '@/Components/SearchBar'
 import HeroCarosuel from '@/Components/HeroCarosuel'
